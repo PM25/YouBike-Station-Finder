90692d35201d1c97de2fa3057d1f75c495250e3e
PM <pm.binary@gmail.com>
2020-04-16 20:58:18 +0800

    Design side information bar.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.onload = main;
 
 function main() {
-    var width = window.innerWidth * 0.65,
+    var width = window.innerWidth,
         height = window.innerHeight;
 
     var svg = d3.select("svg");
@@ -9,32 +9,39 @@ function main() {
     var projection = d3
         .geoMercator()
         .center([121.5654, 25.085]) // 中心點(經緯度)
-        .scale(width * 150) // 放大倍率
-        .translate([width / 2, height / 2]) // 置中
+        .scale(width * 90) // 放大倍率
+        .translate([width / 3, height / 2]) // 置中
         .precision(0.1);
     var path = d3.geoPath().projection(projection);
 
     const zoom = d3.zoom().scaleExtent([0.8, 10]).on("zoom", zoomed);
     svg.call(zoom);
 
+    // d3.json("data/2020-04-11_21-29-13.json").then((data) => {
+    //     console.log(data);
+    // });
+
     d3.json("data/TOWN_MOI_1090324.json").then((taiwan) => {
-        taiwan.features = topojson
+        taipei = topojson
             .feature(taiwan, taiwan.objects.TOWN_MOI_1090324)
             .features.filter(function (data) {
                 // console.log(data.properties.COUNTYNAME);
                 return data.properties.COUNTYNAME == "臺北市";
             });
 
-        // Draw Towns
+        non_taipei = topojson
+            .feature(taiwan, taiwan.objects.TOWN_MOI_1090324)
+            .features.filter(function (data) {
+                // console.log(data.properties.COUNTYNAME);
+                return data.properties.COUNTYNAME != "臺北市";
+            });
+
+        // Draw Taipei City
         svg.selectAll("path")
-            .data(
-                // topojson.feature(taiwan, taiwan.objects.TOWN_MOI_1090324)
-                //     .features
-                taiwan.features
-            )
+            .data(taipei)
             .enter()
             .append("path")
-            .attr("class", "town")
+            .attr("class", "taipei")
             .attr("d", path)
             .attr("id", (data) => {
                 return "city" + data.properties.TOWNID;
@@ -44,18 +51,26 @@ function main() {
                     data.properties.TOWNNAME;
             });
 
+        // Draw Towns that is not Taipei
+        svg.selectAll("path")
+            .data(non_taipei)
+            .enter()
+            .append("path")
+            .attr("class", "non-taipei")
+            .attr("d", path);
+
         // Draw Boundary
         svg.append("path")
             .datum(
                 topojson.mesh(
                     taiwan,
-                    taiwan.objects.TOWN_MOI_1090324,
-                    function (a, b) {
-                        return (
-                            a.properties.COUNTYNAME == "臺北市" ||
-                            b.properties.COUNTYNAME == "臺北市"
-                        );
-                    }
+                    taiwan.objects.TOWN_MOI_1090324
+                    // function (a, b) {
+                    //     return (
+                    //         a.properties.COUNTYNAME == "臺北市" ||
+                    //         b.properties.COUNTYNAME == "臺北市"
+                    //     );
+                    // }
                 )
             )
             .attr("d", path)
